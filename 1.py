47e71a015f37d2401c8164b39828064abdb5e4bc
tjs6595 <t.schultejans@schenckprocess.com>
2022-10-02 11:51:41 -0500

    PPP-8-Sorting-Algorithms-Continued - Activity - Complete without Bonus.

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
 # Write your solution for algorithm 1 below
+def num_sort(num_lst):
+    num_lst.sort()
+    print(num_lst)
+    return
+
+
+
+num_lst = [1, 56, 18, 101, 4, 8, 20]
+num_sort(num_lst)
--- 3.py
+++ 3.py
@@ -1,1 +1,8 @@
-# Write your solution for algorithm 3 below
+# Write your solution for algorithm 2 below
+def num_sorted(num_list):
+    num_list_sorted = sorted(num_list, reverse = True)
+    return num_list_sorted
+
+
+num_lst = [1, 56, 18, 101, 4, 8, 20]
+print(num_sorted(num_lst))
